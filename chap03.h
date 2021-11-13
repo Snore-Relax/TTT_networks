fb161fae86caae8e74b70058d0110d4e0175256a
Snore-Relax <67124641+Snore-Relax@users.noreply.github.com>
2021-11-13 09:39:22 -0800

    Add files via upload

---
+++ chap03.h
@@ -0,0 +1,38 @@
+#if defined(_WIN32)
+
+#ifndef _WIN32_WINNIT  //windows portion 
+#define _WIN32_WINNIT 0x600
+#endif
+
+#include <winsock2.h>
+#include<ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#else
+
+//required for sockets
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+#endif
+
+#if defined(_WIN32)
+#define ISVALIDSOCKET(s) ((s) != INVALID_SOCKET)
+#define CLOSESOCKET(s) closesocket(s)
+#define GETSOCKETERRNO() (WSAGetLastError())
+
+#else
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int 
+#define GETSOCKETERRNO() (errno)
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
