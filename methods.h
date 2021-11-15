74d63cd73b57a68c77d8bfdea30f778025f1014b
Snore-Relax <67124641+Snore-Relax@users.noreply.github.com>
2021-11-14 16:16:19 -0800

    Update methods.h

--- methods.h
+++ methods.h
@@ -13,7 +13,7 @@ char board[3][3]={'1', '2', '3', '4', '5', '6', '7', '8', '9'};
     void switchSymbol();
     bool userInput(int pos);
     int scanWin();
-    void Game();
+    void Game(int sockfd);
 
 
 
